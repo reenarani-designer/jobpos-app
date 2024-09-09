08fd5c2db6bebbdb12655aa6ea11fd0d4b2eab8d
miniayush <45028545+miniayush@users.noreply.github.com>
2024-09-09 12:47:49 +0530

    Filter functionality

--- src/pages/Jobs/Alljobs.js
+++ src/pages/Jobs/Alljobs.js
@@ -3,11 +3,16 @@ import { getAccessToken } from "../../util/Common";
 import { JobCard } from "../../UIComponent/Cards";
 import { Pagination } from "../../UIComponent/Pagination";
 import { config } from "../../util/Configuration";
+import { useSelector } from "react-redux";
+import { CustomCheckBox } from "../../UIComponent/FormControl";
 
 function Alljobs() {
   const token = getAccessToken();
   const [jobs, setJobs] = useState([]);
+  const [filteredJobs, setFilteredJobs] = useState([]);
   const [totalJobs, setTotalJobs] = useState(0);
+  const skills = useSelector((state) => state.skills.skills);
+  const [selectedSkills, setSelectedSkills] = useState([]);
 
   const getJobListing = async () => {
     try {
@@ -25,6 +30,7 @@ function Alljobs() {
 
       const data = await response.json();
       setJobs(data.data.jobs);
+      setFilteredJobs(data.data.jobs);
       setTotalJobs(data.data.total);
     } catch (error) {
       console.error("Error fetching job listings:", error.message);
@@ -32,6 +38,27 @@ function Alljobs() {
       setTotalJobs(0);
       // Handle error display or notification to the user
     }
+  };
+  const filterJobs = (e) => {
+    const value = e.target.value;
+    const newSelectedSkills = selectedSkills.includes(value)
+      ? selectedSkills.filter((skill) => skill !== value)
+      : [...selectedSkills, value];
+    const newFilteredJobs =
+      newSelectedSkills.length > 0
+        ? jobs.filter((job) =>
+            job.skills.some((skill) => newSelectedSkills.includes(skill._id))
+          )
+        : jobs;
+    setSelectedSkills(newSelectedSkills);
+    setFilteredJobs(newFilteredJobs);
+    setTotalJobs(newFilteredJobs.length);
+  };
+
+  const resetSkills = () => {
+    setSelectedSkills([]);
+    setFilteredJobs(jobs);
+    setTotalJobs(jobs.length);
   };
 
   useEffect(() => {
@@ -70,50 +97,34 @@ function Alljobs() {
         <div className="row">
           <div className="col-sm-4">
             <aside className="bg-light p-3">
-              <h3 className="h4">Filter</h3>
-              <label htmlFor="keyword" className="form-label">
-                Keyword
-              </label>
-              <input
-                type="text"
-                className="form-control mb-3"
-                id="keyword"
-                aria-describedby="keyword"
-                placeholder="Search for service"
-              />
-              <label htmlFor="jobcat" className="form-label">
-                Job Category
-              </label>
-              <select className="form-select mb-3" aria-label="Job Category">
-                <option defaultValue={1}>All Categories</option>
-                <option value={2}>Cleaning</option>
-                <option value={3}>Plumber</option>
-                <option value={4}>Electrician</option>
-                <option value={5}>Carpenter</option>
-                <option value={6}>Painting</option>
-              </select>
-              <label htmlFor="jobtype" className="form-label">
-                Job Type
-              </label>
-              <select className="form-select mb-3" aria-label="Job Type">
-                <option defaultValue={1}>All Jobs</option>
-                <option value={2}>Full Time</option>
-                <option value={3}>Part Time</option>
-                <option value={4}>Fixed Term</option>
-              </select>
-              <button type="submit" className="btn btn-primary">
-                Apply Filter
-              </button>
-              <button type="reset" className="btn btn-secondary ms-2">
-                Cancel
+              <h3 className="h4">Skills</h3>
+              <div className="d-inline-column mb-3">
+                {skills &&
+                  skills.map((skill) => (
+                    <CustomCheckBox
+                      key={skill._id}
+                      label={skill.name}
+                      id={skill._id}
+                      value={skill._id}
+                      onChange={filterJobs}
+                      preSelectedList={selectedSkills}
+                    />
+                  ))}
+              </div>
+              <button
+                type="reset"
+                className="btn btn-secondary ms-2"
+                onClick={resetSkills}
+              >
+                Clear
               </button>
             </aside>
           </div>
 
           <div className="col-sm-8">
             {/* Job Listings */}
-            {jobs.length > 0 ? (
-              jobs.map((jobDetails) => (
+            {filteredJobs.length > 0 ? (
+              filteredJobs.map((jobDetails) => (
                 <JobCard key={jobDetails._id} jobDetails={jobDetails} />
               ))
             ) : (
@@ -121,7 +132,11 @@ function Alljobs() {
             )}
 
             {/* Pagination */}
-            <Pagination totalRecords={totalJobs} currentPage={1} pageLimit={10} />
+            <Pagination
+              totalRecords={totalJobs}
+              currentPage={1}
+              pageLimit={10}
+            />
           </div>
         </div>
       </div>
